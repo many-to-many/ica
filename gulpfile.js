397c684765cba7448bc00974c9c721c64ae1fc5f
Zhuo Lu <sethlu@mintkit.net>
2017-02-18 23:01:07 -0800

    Trim output JavaScript file

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,51 @@
 
-var gulp = require('gulp');
-var compass = require('gulp-compass');
-var concat = require('gulp-concat');
-var browserSync = require('browser-sync').create();
-var config = require('./config.json');
+var gulp = require("gulp");
+var compass = require("gulp-compass");
+var concat = require("gulp-concat");
+var strip = require("gulp-strip-comments");
+var browserSync = require("browser-sync").create();
+var config = require("./config.json");
 
-gulp.task('compass', function() {
-  return gulp.src('styles/**/*.scss')
+gulp.task("compass", function() {
+  return gulp.src("styles/**/*.scss")
     .pipe(compass({
-      css: 'styles',
-      sass: 'styles/sass'
-    }))
+      css: "styles",
+      sass: "styles/sass"
+    }));
 });
 
-gulp.task('css', ['compass'], function () {
-  return gulp.src('styles/**/*.css')
-    .pipe(gulp.dest('app/assets'));
+gulp.task("css", ["compass"], function () {
+  return gulp.src("styles/**/*.css")
+    .pipe(gulp.dest("app/assets"));
 });
 
-gulp.task('css-watch', ['css'], function () {
-  browserSync.reload('*.css');
+gulp.task("css-watch", ["css"], function () {
+  browserSync.reload("*.css");
 });
 
-gulp.task('js', function () {
-  return gulp.src(require('./scripts.json'))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('app/assets'));
+gulp.task("js", function () {
+  return gulp.src(require("./scripts.json"))
+    .pipe(concat("main.js"))
+    .pipe(strip({trim: true}))
+    .pipe(gulp.dest("app/assets"));
 });
 
-gulp.task('js-watch', ['js'], function () {
-  browserSync.reload('*.js');
+gulp.task("js-watch", ["js"], function () {
+  browserSync.reload("*.js");
 });
 
-gulp.task('build', ['compass', 'css', 'js']);
+gulp.task("build", ["compass", "css", "js"]);
 
-gulp.task('proxy', ['build'], function () {
+gulp.task("proxy", ["build"], function () {
   browserSync.init({
     proxy: config.proxy
   });
 
-  gulp.watch('**/*.html').on("change", function () {
+  gulp.watch("**/*.html").on("change", function () {
     browserSync.reload();
   });
-  gulp.watch('styles/**/*.scss', ['css-watch']);
-  gulp.watch('scripts/**/*.js', ['js-watch']);
+  gulp.watch("styles/**/*.scss", ["css-watch"]);
+  gulp.watch("scripts/**/*.js", ["js-watch"]);
 });
 
-gulp.task('default', ['proxy']);
+gulp.task("default", ["proxy"]);
