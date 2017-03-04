503f8e554ff7e31cb1eef208ebad7d453b7f5008
Zhuo Lu <sethlu@mintkit.net>
2017-03-03 17:36:31 -0800

    Fix code style

--- scripts/explore/ExploreController.js
+++ scripts/explore/ExploreController.js
@@ -14,20 +14,20 @@ ExploreController.defineMethod("updateView", function updateView() {
 
   this.explore.items.reverse().map(function (item) {
     switch (item.constructor) {
-      case JointSource:
-        var jointSource = item;
-        // Check existing element
-        if (this.querySelector("[data-ica-jointsource-id='{0}']".format(jointSource.jointSourceId))) break;
+    case JointSource:
+      var jointSource = item;
+      // Check existing element
+      if (this.querySelector("[data-ica-jointsource-id='{0}']".format(jointSource.jointSourceId))) break;
 
-        // Create new view
-        var fragment = ExploreJointSourceController.createViewFragment(jointSource);
-        var element = fragment.querySelector(".jointsource");
-        this.insertBefore(fragment, this.firstChild);
-        new ExploreJointSourceController(jointSource, element).componentOf = this.controller;
+      // Create new view
+      var fragment = ExploreJointSourceController.createViewFragment(jointSource);
+      var element = fragment.querySelector(".jointsource");
+      this.insertBefore(fragment, this.firstChild);
+      new ExploreJointSourceController(jointSource, element).componentOf = this.controller;
 
-        break;
-      default:
-        console.warn("Unhandled item:", item.constructor);
+      break;
+    default:
+      console.warn("Unhandled item:", item.constructor);
     }
   }.bind(this.view));
 });
