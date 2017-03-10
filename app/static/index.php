805a539ae5c00772db886a43310f936574fd73c9
Zhuo Lu <sethlu@mintkit.net>
2017-03-10 13:30:54 -0800

    Update image resizing & conversion mechanism
    - Future support CDN caching image files with file extensions explicitly in URL link
    - Future support resampling from/to different image types

--- app/static/index.php
+++ app/static/index.php
@@ -2,53 +2,90 @@
 
   require_once(__DIR__ . "/init.php");
 
-  if (list($fileId) = handle([REQUEST_PARAMETER], true)) {
-    $file = \ICA\Files\getFile($fileId);
+  if (list($requestFileName) = handle([REQUEST_PARAMETER], true)) {
+    list($requestFileId, $requestFileExt) = explode(".", $requestFileName);
+    $file = \ICA\Files\getFile($requestFileId);
     if (!$file) throw new Exception("File not found");
 
     preg_match("/([^\/]+)\/([^\/]+)/", $file->mime, $matches);
+    $fileType = $matches[1];
+    $fileSubtype = $matches[2];
 
-    switch ($matches[1]) {
+    switch ($fileType) {
       case "image":
-        // Check if request needs image resizing
-        if (isset($_GET["width"]) || isset($_GET["height"])) {
-          $width = isset($_GET["width"]) ? intval($_GET["width"]) : 0;
-          $height = isset($_GET["height"]) ? intval($_GET["height"]) : 0;
 
-          $path = DIR_ROOT . "/cache/{$file->path}/{$width}x{$height}";
-          // Resize the image if not already made available
-          $imageFunctionPairs = [
-            "jpeg" => ["imagecreatefromjpeg", "imagejpeg"]
-          ];
+        // Functions for image manipulation
+        $imageCreateFunctions = [
+          "jpeg" => "imagecreatefromjpeg",
+        ];
+        $imageFunctions = [
+          "jpeg" => "imagejpeg"
+        ];
+        $requestImageSubtypesByFileExt = [
+          "jpg" => "jpeg",
+          "jpeg" => "jpeg"
+        ];
 
-          if (file_exists($path)) {
-            respondFile($path, $file->mime);
-          } elseif (array_key_exists($matches[2], $imageFunctionPairs)) {
-            $src = $imageFunctionPairs[$matches[2]][0](DIR_ROOT . "/data/{$file->path}");
+        if (empty($requestFileExt)) {
+          $requestImageSubtype = $fileSubtype;
+        } elseif (array_key_exists($requestFileExt, $requestImageSubtypesByFileExt)) {
+          $requestImageSubtype = $requestImageSubtypesByFileExt[$requestFileExt];
+        } else {
+          throw new Exception("Unsupported file extension");
+        }
 
-            list($imageWidth, $imageHeight) = getimagesize(DIR_ROOT . "/data/{$file->path}");
-            if ($width <= 0 && $height > 0) {
-              $width = $imageWidth * $height / $imageHeight;
-            } elseif ($height <= 0 && $width > 0) {
-              $height= $width * $imageHeight / $imageWidth;
-            } elseif ($width <= 0 && $height <= 0) {
-              $width = $imageWidth;
-              $height = $imageHeight;
-            }
-            $dst = imagecreatetruecolor($width, $height);
-            imagecopyresampled($dst, $src, 0, 0, 0, 0, $width, $height, $imageWidth, $imageHeight);
+        $width = isset($_GET["width"]) ? intval($_GET["width"]) : 0;
+        $height = isset($_GET["height"]) ? intval($_GET["height"]) : 0;
 
-            if (!is_dir(DIR_ROOT . "/cache/{$file->path}")
-              && !mkdir(DIR_ROOT . "/cache/{$file->path}", 0755, true)) {
-              throw new Exception("Unable to create directory structure");
-            }
+        // Path to extension-specific image cache
+        $path = DIR_ROOT . "/cache/{$file->path}/{$width}x{$height}-{$requestImageSubtype}";
 
-            $imageFunctionPairs[$matches[2]][1]($dst, $path);
-            respondFile($path, $file->mime);
+        if (file_exists($path)) {
+
+          // Responds the file if already available
+          respondFile($path, "image/{$requestImageSubtype}");
+
+        } elseif (
+          // If the image is convertible
+          array_key_exists($fileSubtype, $imageCreateFunctions)
+          && array_key_exists($requestImageSubtype, $imageFunctions)
+        ) {
+
+          // Convert and resize the image if not already made available
+          $src = $imageCreateFunctions[$fileSubtype](DIR_ROOT . "/data/{$file->path}");
+
+          list($imageWidth, $imageHeight) = getimagesize(DIR_ROOT . "/data/{$file->path}");
+          if ($width <= 0 && $height > 0) {
+            $width = $imageWidth * $height / $imageHeight;
+          } elseif ($height <= 0 && $width > 0) {
+            $height= $width * $imageHeight / $imageWidth;
+          } elseif ($width <= 0 && $height <= 0) {
+            $width = $imageWidth;
+            $height = $imageHeight;
           }
+          $dst = imagecreatetruecolor($width, $height);
+          imagecopyresampled($dst, $src, 0, 0, 0, 0, $width, $height, $imageWidth, $imageHeight);
+
+          if (!is_dir(DIR_ROOT . "/cache/{$file->path}")
+            && !mkdir(DIR_ROOT . "/cache/{$file->path}", 0755, true)) {
+            throw new Exception("Unable to create directory for image");
+          }
+
+          $imageFunctions[$requestImageSubtype]($dst, $path);
+          respondFile($path, $file->mime);
+
+        } elseif ($requestImageSubtype == $fileSubtype) {
+
+          // Image cannot be resampled, but type conversion unncessary
+          // Serve original file
+          break;
+
+        } else {
+
+          throw new Exception("Unsupported image type conversion");
+
         }
     }
-
 
     // Serve original file
     respondFile(DIR_ROOT . "/data/{$file->path}", $file->mime);
