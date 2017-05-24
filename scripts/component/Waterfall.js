e0b49684eefabdfd039aa9ae32e5a58058ea5013
Zhuo Lu <sethlu@mintkit.net>
2017-05-24 18:10:46 +0800

    Draft Waterfall for timed transitions

---
+++ scripts/component/Waterfall.js
@@ -0,0 +1,39 @@
+
+var Waterfall = Component.createComponent("Waterfall");
+
+Waterfall.defineMethod("construct", function () {
+  // Construct private container
+  Object.defineProperty(this, "_container", {
+    value: {
+      then: []
+    }
+  });
+});
+
+Waterfall.defineMethod("init", function (func, duration = 10, start = true) {
+  // Init execute method
+  var container = this._container;
+  this._execute = function () {
+    if (func) func();
+
+    setTimeout(function () {
+      container.then.forEach(function (item) {
+        item.start();
+      });
+    }, duration);
+  };
+  // Start waterfall
+  if (start) this.start();
+  return [];
+});
+
+Waterfall.prototype.start = function () {
+  // Execute the function
+  this._execute();
+};
+
+Waterfall.prototype.then = function (func, duration) {
+  var next = new Waterfall(func, duration, false);
+  this._container.then.push(next);
+  return next;
+};
