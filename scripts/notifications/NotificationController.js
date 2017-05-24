721bd72c855e99a732570ea6e937f4e49284dc62
Zhuo Lu <sethlu@mintkit.net>
2017-05-24 22:45:58 +0800

    Promote notification controller destroy method

--- scripts/notifications/NotificationController.js
+++ scripts/notifications/NotificationController.js
@@ -2,3 +2,18 @@
 var NotificationController = SingleModelController.createComponent("NotificationController");
 
 NotificationController.defineAlias("model", "notification");
+
+NotificationController.defineMethod("destroy", function (destroyView = false) {
+  // Destroy view
+  if (destroyView && this.view) {
+    var view = this.view;
+    new Waterfall(null, 400) // Leave time for transition to finish
+      .then(function () {
+        view.classList.add("hidden");
+      }, 400)
+      .then(function () {
+        view.parentNode.removeChild(view);
+      }.bind(this));
+  }
+  return [];
+});
--- scripts/notifications/XHRProgressNotificationController.js
+++ scripts/notifications/XHRProgressNotificationController.js
@@ -15,18 +15,3 @@ XHRProgressNotificationController.defineMethod("updateView", function () {
     this.notification.destroy(true, true);
   }
 });
-
-XHRProgressNotificationController.defineMethod("destroy", function (destroyView = false) {
-  // Destroy view
-  if (destroyView && this.view) {
-    var view = this.view;
-    new Waterfall(null, 400) // Leave time for transition to finish
-      .then(function () {
-        view.classList.add("hidden");
-      }, 400)
-      .then(function () {
-        view.parentNode.removeChild(view);
-      }.bind(this));
-  }
-  return [];
-});
