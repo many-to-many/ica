3e32a1323eb0f2f0a9c0b277511ef4a4a1e87eff
Zhuo Lu <sethlu@mintkit.net>
2017-06-04 02:32:31 +0800

    Default message to undefined

--- scripts/notifications/BasicNotification.js
+++ scripts/notifications/BasicNotification.js
@@ -1,7 +1,7 @@
 
 var BasicNotification = Notification.createComponent("BasicNotification");
 
-BasicNotification.defineMethod("init", function init(title, message = null, duration = 3000) {
+BasicNotification.defineMethod("init", function init(title, message, duration = 3000) {
 
   this.title = title;
   this.message = message;
