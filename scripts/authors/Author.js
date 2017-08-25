0409b89fa40fe33023f188f6141087388584d5fc
Zhuo Lu <sethlu@mintkit.net>
2017-08-25 14:22:31 -0700

    Review and annotate Author

--- scripts/authors/Author.js
+++ scripts/authors/Author.js
@@ -1,19 +1,28 @@
 
-var Author = Model.createComponent("Author");
+let Author = Model.createComponent("Author");
 
 Author.authors = {};
 
-Author.defineMethod("init", function init(authorId) {
+Author.defineMethod("init", function init(authorId, name) {
+
   // Init authorId
   this._authorId = authorId;
   this.initAuthorId();
+
+  // Init name
+  this._name = name;
+
   return [];
 });
 
 Author.defineMethod("uninit", function uninit() {
+
+  // Uninit name
+
   // Uninit authorId
   this.uninitAuthorId();
   delete this._authorId;
+
 });
 
 // AuthorId
@@ -23,7 +32,7 @@ Object.defineProperty(Author.prototype, "authorId", {
     return this._authorId;
   },
   set: function (value) {
-    if (this._authorId == value) return;
+    if (value === this._authorId) return;
     this.uninitAuthorId();
     this._authorId = value;
     this.initAuthorId();
@@ -38,4 +47,16 @@ Author.defineMethod("initAuthorId", function initAuthorId() {
 Author.defineMethod("uninitAuthorId", function uninitAuthorId() {
   if (!this.authorId) return;
   delete Author.authors[this.authorId];
+});
+
+// Name
+
+Object.defineProperty(Author.prototype, "name", {
+  get: function () {
+    return this._name || "Anonymous";
+  },
+  set: function (value) {
+    if (value === this._name) return;
+    this._name = value;
+  }
 });
