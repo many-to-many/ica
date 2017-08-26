bad8ea08cd159d9e6e97ff19cd6589e076ede520
Zhuo Lu <sethlu@mintkit.net>
2017-08-26 15:43:52 -0700

    Review and annotate AudioSource

--- scripts/jointsources/AudioSource.js
+++ scripts/jointsources/AudioSource.js
@@ -1,9 +1,15 @@
 
-var AudioSource = BlobFileSource.createComponent("AudioSource");
+/**
+ * AudioSource
+ * Concrete model for a piece of audio recording.
+ */
+let AudioSource = BlobFileSource.createComponent("AudioSource");
 
 AudioSource.defineMethod("construct", function construct() {
 
+  // Construct file handler
+  // This will be destroyed by BlobFileSource
   if (!this.fileHandler) this.fileHandler = new BlobFileHandler();
-  this.fileHandler.cached = false;
+  this.fileHandler.cached = false; // Audio files are not cached
 
 });
