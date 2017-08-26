74a56c6455a93b71cd5b6c8618f7290c84955182
Zhuo Lu <sethlu@mintkit.net>
2017-08-26 02:15:20 -0700

    Annotate and review Explore

--- scripts/explore/Explore.js
+++ scripts/explore/Explore.js
@@ -1,37 +1,52 @@
 
-var Explore = Model.createComponent("Explore");
+/**
+ * Explore
+ * Represents an unordered list of articles (JointSources) for the view controller to display. The view controller may choose to ignore the order given by the array and impose an arbitrary display order.
+ * @constructor
+ */
+let Explore = Model.createComponent("Explore");
 
 Explore.explores = {count: 0};
 
 Explore.defineMethod("construct", function construct() {
+
   // Construct exploreId
   Object.defineProperty(this, "exploreId", {
     value: ++Explore.explores.count
   });
   Explore.explores[this.exploreId] = this;
+
 });
 
 Explore.defineMethod("init", function init(items) {
+
   // Init items
   this.items = items || [];
+
   return [];
 });
 
 Explore.defineMethod("uninit", function uninit() {
+
   // Uninit items
   delete this.items;
+
 });
 
 Explore.defineMethod("destruct", function destruct() {
+
   // Destruct exploreId
   delete Explore.explores[this.exploreId];
+
 });
 
-Explore.prototype.addItems = function (items) {
+// Items
+
+Explore.prototype.addItems = function addItems(items) {
   Array.prototype.push.apply(this.items, items);
 };
 
-Explore.prototype.putItems = function (items) {
+Explore.prototype.putItems = function putItems(items) {
   this.items.splice(0, this.items.length);
   this.addItems(items);
 };
