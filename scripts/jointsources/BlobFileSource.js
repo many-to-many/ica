87039a075694722fa41dc85b9c6bf7ee024d7038
Zhuo Lu <sethlu@mintkit.net>
2017-08-26 15:56:03 -0700

    Review and annotate BlobFileSource

--- scripts/jointsources/BlobFileSource.js
+++ scripts/jointsources/BlobFileSource.js
@@ -1,14 +1,20 @@
 
-var BlobFileSource = Source.createComponent("BlobFileSource");
+/**
+ * BlobFileSource
+ * Abstract model for a media file that can be audio, video, image, doc, etc.
+ */
+let BlobFileSource = Source.createComponent("BlobFileSource");
 
 BlobFileSource.defineMethod("construct", function construct() {
 
+  // Construct file handler
   if (!this.fileHandler) this.fileHandler = new BlobFileHandler();
 
 });
 
-BlobFileSource.defineMethod("init", function init(content, conversation, sourceId) {
+BlobFileSource.defineMethod("init", function init(content, jointSource, sourceId) {
 
+  // Sync file handler
   if (content && content["0"]) {
     this.fileHandler.blob = content["0"];
     this.fileHandler.didUpdate();
@@ -18,6 +24,7 @@ BlobFileSource.defineMethod("init", function init(content, conversation, sourceI
 
 BlobFileSource.defineMethod("didUpdate", function didUpdate() {
 
+  // Sync file handler
   this.fileHandler.blob = this.content["0"];
   this.fileHandler.didUpdate();
 
@@ -25,11 +32,15 @@ BlobFileSource.defineMethod("didUpdate", function didUpdate() {
 
 BlobFileSource.defineMethod("destruct", function destruct() {
 
+  // Destruct file handler
   this.fileHandler.destroy();
+  delete this.fileHandler;
 
 });
 
-BlobFileSource.prototype.prePublish = function () {
+// Publish
+
+BlobFileSource.prototype.prePublish = function prePublish() {
   return Promise.resolve()
     .then(function () {
       if (this.content["0"] && this.content["0"] instanceof Blob) {
@@ -41,7 +52,9 @@ BlobFileSource.prototype.prePublish = function () {
     }.bind(this));
 };
 
-BlobFileSource.prototype.getFileStats = function () {
+// Stats
+
+BlobFileSource.prototype.getFileStats = function getFileStats() {
   if (this.content["0"]) {
     if (this.content["0"] instanceof Blob) {
       return Promise.resolve({
@@ -51,5 +64,5 @@ BlobFileSource.prototype.getFileStats = function () {
     }
     return ICA.getFileStats(this.content["0"]);
   }
-  return Promise.reject(new Error("File not exist"));
+  return Promise.reject(new Error("File does not exist"));
 };
