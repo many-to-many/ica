e1e3dfd8f4cae1beb47c585d1ec2cff082218bb0
Zhuo Lu <sethlu@mintkit.net>
2017-08-26 15:34:01 -0700

    Review and annotate ExploreDiscussionController

--- scripts/explore/ExploreDiscussionController.js
+++ scripts/explore/ExploreDiscussionController.js
@@ -1,38 +1,65 @@
 
-var ExploreDiscussionController = ConversationController.createComponent("ExploreDiscussionController");
+/**
+ * ExploreDiscussionController
+ * Concrete view controller to display a Discussion.
+ * @constructor
+ */
+let ExploreDiscussionController = ConversationController.createComponent("ExploreDiscussionController");
 
-ExploreDiscussionController.createViewFragment = function () {
+ExploreDiscussionController.createViewFragment = function createViewFragment() {
   return cloneTemplate("#template-explore-discussion");
 };
 
 ExploreDiscussionController.defineAlias("model", "discussion");
 
-ExploreDiscussionController.defineMethod("initView", function initView() {
-  if (!this.view) return;
+(function (ExploreDiscussionController) {
 
-  setElementProperty(this.view, "discussion-id", this.discussion.discussionId);
-  this.view.style.order = -this.discussion.discussionId;
+  ExploreDiscussionController.defineMethod("initView", function initView() {
+    if (!this.view) return;
 
-  this.view.addEventListener("click", function (e) {
+    // Init click to display full article
+
+    this.view.addEventListener("click", viewOnClick);
+
+    setElementProperty(this.view, "discussion-id", this.discussion.discussionId);
+  });
+
+  ExploreDiscussionController.defineMethod("updateView", function updateView() {
+    if (!this.view) return;
+
+    // Update display metadata
+
+    this.view.querySelectorAll("[data-ica-discussion]").forEach(function (element) {
+      element.textContent = this.discussion[getElementProperty(element, "discussion")]["0"] || "";
+    }.bind(this));
+
+    // Display order
+
+    this.view.style.order = -this.discussion.discussionId;
+
+  });
+
+  ExploreDiscussionController.defineMethod("uninitView", function uninitView() {
+    if (!this.view) return;
+
+    // Uninit click to display full article
+
+    this.view.removeEventListener("click", viewOnClick);
+
+    removeElementProperty(this.view, "discussion-id");
+  });
+
+  // Shared functions
+
+  function viewOnClick() {
     e.preventDefault();
     e.stopPropagation();
-    var fragment = MapArticleDiscussionController.createViewFragment();
-    var element = fragment.querySelector(".article-container");
+
+    let fragment = MapArticleDiscussionController.createViewFragment();
+    let element = fragment.querySelector(".article-container");
+
     document.body.querySelector(".app-view").appendChild(fragment);
     new MapArticleDiscussionController(this.controller.discussion, element);
-  }.bind(this.view));
-});
+  }
 
-ExploreDiscussionController.defineMethod("updateView", function updateView() {
-  if (!this.view) return;
-
-  this.view.querySelectorAll("[data-ica-discussion]").forEach(function (element) {
-    element.textContent = this.discussion[getElementProperty(element, "discussion")]["0"] || "";
-  }.bind(this));
-});
-
-ExploreDiscussionController.defineMethod("uninitView", function uninitView() {
-  if (!this.view) return;
-
-  removeElementProperty(this.view, "discussion-id");
-});
+})(ExploreDiscussionController);
