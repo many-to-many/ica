ed31a7c7c2e679b87b3489434a76ef50d6b748b5
Zhuo Lu <sethlu@mintkit.net>
2017-09-01 02:11:47 -0700

    Review and annotate VideoSource

--- scripts/jointsources/VideoSource.js
+++ scripts/jointsources/VideoSource.js
@@ -1,9 +1,15 @@
 
-var VideoSource = BlobFileSource.createComponent("VideoSource");
+/**
+ * VideoSource
+ * Concrete model for a video clip.
+ */
+let VideoSource = BlobFileSource.createComponent("VideoSource");
 
 VideoSource.defineMethod("construct", function construct() {
 
+  // Construct file handler
+  // This will be destroyed by BlobFileSource
   if (!this.fileHandler) this.fileHandler = new BlobFileHandler();
-  this.fileHandler.cached = false;
+  this.fileHandler.cached = false; // Video files are not cached
 
 });
