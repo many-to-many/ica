6310d988952e293f7341630f0b7b16599ffd2e26
Zhuo Lu <sethlu@mintkit.net>
2017-09-08 18:44:05 -0700

    Review and annotate MapConversationImageSourceController

--- scripts/map/MapConversationImageSourceController.js
+++ scripts/map/MapConversationImageSourceController.js
@@ -1,14 +1,19 @@
 
-var MapConversationImageSourceController = MapConversationSourceController.createComponent("MapConversationImageSourceController");
+/**
+ * MapConversationImageSourceController
+ * Concrete view controller to display an image source.
+ * @constructor
+ */
+let MapConversationImageSourceController = MapConversationSourceController.createComponent("MapConversationImageSourceController");
 
-MapConversationImageSourceController.createViewFragment = function (source) {
+MapConversationImageSourceController.createViewFragment = function () {
   return cloneTemplate("#template-map-conversation-imagesource");
 };
 
-MapConversationImageSourceController.defineMethod("updateView", function updateView(length = 0) {
+MapConversationImageSourceController.defineMethod("updateView", function updateView() {
   if (!this.view) return;
 
-  var img = this.view.querySelector("img[data-ica-content]");
+  let img = this.view.querySelector("img[data-ica-content]");
   img.src = this.source.content["0"]
     ? this.source.fileHandler.blob instanceof Blob
       ? this.source.fileHandler.url
