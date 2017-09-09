ae0a23cac830156b9820ca159c1bbc5d004627c1
Zhuo Lu <sethlu@mintkit.net>
2017-09-08 18:55:44 -0700

    Review and annotate MapConversationTextSourceController

--- scripts/map/MapConversationTextSourceController.js
+++ scripts/map/MapConversationTextSourceController.js
@@ -1,9 +1,16 @@
 
+/**
+ * MapConversationTextSourceController
+ * Concrete view controller to display a text source.
+ * @constructor
+ */
 let MapConversationTextSourceController = MapConversationSourceController.createComponent("MapConversationTextSourceController");
 
 MapConversationTextSourceController.createViewFragment = function () {
   return cloneTemplate("#template-map-conversation-textsource");
 };
+
+// View
 
 MapConversationTextSourceController.defineMethod("initView", function initView() {
   if (!this.view) return;
@@ -11,10 +18,19 @@ MapConversationTextSourceController.defineMethod("initView", function initView()
   this.quill = new Quill(this.view.querySelector(".text"), {
     readOnly: true
   });
+
 });
 
 MapConversationTextSourceController.defineMethod("updateView", function updateView() {
   if (!this.view) return;
 
   this.quill.setText(this.source.content);
+
+});
+
+MapConversationTextSourceController.defineMethod("uninitView", function unitView() {
+  if (!this.view) return;
+
+  delete this.quill;
+
 });
