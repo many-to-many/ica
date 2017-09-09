17928d5e4d4593a4fd00a16375b2430b67d3ef0a
Zhuo Lu <sethlu@mintkit.net>
2017-09-08 20:11:00 -0700

    Review and annotate MapDiscussionController

--- scripts/map/MapDiscussionController.js
+++ scripts/map/MapDiscussionController.js
@@ -1,4 +1,9 @@
 
+/**
+ * MapDiscussionController
+ * Concrete view controller to display a Discussion.
+ * @constructor
+ */
 const MapDiscussionController = DiscussionController.createComponent("MapDiscussionController");
 
 MapDiscussionController.createViewFragment = function () {
@@ -7,29 +12,40 @@ MapDiscussionController.createViewFragment = function () {
 
 // View
 
-MapDiscussionController.defineMethod("initView", function initView() {
-  if (!this.view) return;
+(function (MapDiscussionController) {
 
-  this.view.addEventListener("click", function (e) {
-    e.stopPropagation();
-  }.bind(this.view));
+  MapDiscussionController.defineMethod("initView", function initView() {
+    if (!this.view) return;
 
-});
+    this.view.addEventListener("click", viewOnClick);
 
-MapDiscussionController.defineMethod("updateView", function updateView() {
-  if (!this.view) return;
+  });
 
-  this.view.querySelectorAll("[data-ica-discussion-predicate]").forEach(function (element) {
-    let predicate = getElementProperty(element, "discussion-predicate");
-    if (isEmpty(this.discussion[predicate])) {
-      element.style.display = "none";
-    } else {
-      element.style.display = "";
-    }
-  }.bind(this));
+  MapDiscussionController.defineMethod("updateView", function updateView() {
+    if (!this.view) return;
 
-  this.view.querySelectorAll("[data-ica-discussion]").forEach(function (element) {
-    element.textContent = this.discussion[getElementProperty(element, "discussion")]["0"];
-  }.bind(this));
+    this.view.querySelectorAll("[data-ica-discussion-predicate]").forEach(function (element) {
+      let predicate = getElementProperty(element, "discussion-predicate");
+      element.style.display = isEmpty(this.discussion[predicate]) ? "none" : "";
+    }.bind(this));
 
-});
+    this.view.querySelectorAll("[data-ica-discussion]").forEach(function (element) {
+      element.textContent = this.discussion[getElementProperty(element, "discussion")]["0"];
+    }.bind(this));
+
+  });
+
+  MapDiscussionController.defineMethod("uninitView", function uninitView() {
+    if (!this.view) return;
+
+    this.view.removeEventListener("click", viewOnClick);
+
+  });
+
+  // Shared functions
+
+  function viewOnClick(event) {
+    event.stopPropagation();
+  }
+
+})(MapDiscussionController);
