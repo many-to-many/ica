353eb3df4ec25ec9f1d5d472f256e15b679191bb
Zhuo Lu <sethlu@mintkit.net>
2017-09-08 18:50:38 -0700

    Review and annotate MapConversationSourceController

--- scripts/map/MapConversationSourceController.js
+++ scripts/map/MapConversationSourceController.js
@@ -1,19 +1,44 @@
 
-var MapConversationSourceController = SourceController.createComponent("MapConversationSourceController");
+/**
+ * MapConversationSourceController
+ * Abstract view controller to display a source model.
+ * @constructor
+ */
+let MapConversationSourceController = SourceController.createComponent("MapConversationSourceController");
 
-MapConversationSourceController.defineMethod("initView", function initView() {
-  if (!this.view) return;
+(function (MapConversationSourceController) {
 
-  let editConversationAnchor = this.view.querySelector("[data-ica-action='edit-conversation']");
-  editConversationAnchor.href = "/conversations/{0}/edit".format(this.source.jointSource.conversationId);
-  editConversationAnchor.addEventListener("click", function (event) {
+  MapConversationSourceController.defineMethod("initView", function initView() {
+    if (!this.view) return;
+
+    this.view.addEventListener("click", viewOnClick);
+
+    let editConversationAnchor = this.view.querySelector("[data-ica-action='edit-conversation']");
+    editConversationAnchor.href = "/conversations/{0}/edit".format(this.source.jointSource.conversationId);
+    editConversationAnchor.addEventListener("click", editConversationAnchorOnClick);
+    editConversationAnchor.controller = this;
+
+  });
+
+  MapConversationSourceController.defineMethod("uninitView", function uninitView() {
+    if (!this.view) return;
+
+    this.view.querySelector("[data-ica-action='edit-conversation']").removeEventListener("click", editConversationAnchorOnClick);
+
+    this.view.removeEventListener("click", viewOnClick);
+
+  });
+
+  // Shared functions
+
+  function viewOnClick(event) {
+    event.stopPropagation();
+  }
+
+  function editConversationAnchorOnClick(event) {
     event.preventDefault();
 
     this.controller.componentOf.displayPublisherConversationView();
-  }.bind(this.view));
+  }
 
-  this.view.addEventListener("click", function (e) {
-    // e.preventDefault();
-    e.stopPropagation();
-  });
-});
+})(MapConversationSourceController);
