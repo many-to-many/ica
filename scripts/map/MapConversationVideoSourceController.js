acd391219f54067a22254304cca9d052c754bfc4
Zhuo Lu <sethlu@mintkit.net>
2017-09-08 19:09:23 -0700

    Review and annotate MapConversationVideoSourceController

--- scripts/map/MapConversationVideoSourceController.js
+++ scripts/map/MapConversationVideoSourceController.js
@@ -1,30 +1,36 @@
 
-var MapConversationVideoSourceController = MapConversationSourceController.createComponent("MapConversationVideoSourceController");
+/**
+ * MapConversationVideoSourceController
+ * Concrete view controller to display video source.
+ * @constructor
+ */
+let MapConversationVideoSourceController = MapConversationSourceController.createComponent("MapConversationVideoSourceController");
 
-MapConversationVideoSourceController.createViewFragment = function (source) {
+MapConversationVideoSourceController.createViewFragment = function () {
   return cloneTemplate("#template-map-conversation-videosource");
 };
 
 MapConversationVideoSourceController.defineMethod("updateView", function updateView() {
   if (!this.view) return;
 
-  var displayPlayer = function displayPlayer(display) {
+  let displayPlayer = function displayPlayer(display) {
     this.querySelector(".plyr").hidden = !display;
   }.bind(this.view);
 
   displayPlayer(false);
   this.source.getFileStats()
     .then(function (stats) {
-      if (this.querySelector("video").canPlayType(stats.type) != "") {
+      if (this.querySelector("video").canPlayType(stats.type) !== "") {
         displayPlayer(true);
       }
     }.bind(this.view));
 
-  if (this.source.fileHandler.blob) {
-    this.view.querySelector("source[data-ica-content='0']").src = this.source.fileHandler.url;
+  let url = this.source.fileHandler.url;
+  if (url) {
+    this.view.querySelector("source[data-ica-content='0']").src = url;
 
-    this.view.querySelector("a[data-ica-content='0']").href = this.source.fileHandler.url;
-    this.view.querySelector("a[data-ica-content='0']").textContent = this.source.fileHandler.url;
+    this.view.querySelector("a[data-ica-content='0']").href = url;
+    this.view.querySelector("a[data-ica-content='0']").textContent = url;
   }
 
 });
