c5b280b09702b52c9a5230ede92e4422c1443725
Zhuo Lu <sethlu@mintkit.net>
2017-09-08 17:08:17 -0700

    Review and annotate MapConversationAudioSourceController

--- scripts/map/MapConversationAudioSourceController.js
+++ scripts/map/MapConversationAudioSourceController.js
@@ -1,39 +1,62 @@
 
+/**
+ * MapConversationAudioSourceController
+ * Concrete view controller to display an audio source.
+ * @constructor
+ */
 let MapConversationAudioSourceController = MapConversationSourceController.createComponent("MapConversationAudioSourceController");
 
-MapConversationAudioSourceController.createViewFragment = function (source) {
+MapConversationAudioSourceController.createViewFragment = function () {
   return cloneTemplate("#template-map-conversation-audiosource");
 };
+
+// View
 
 MapConversationAudioSourceController.defineMethod("initView", function initView() {
   if (!this.view) return;
 
+  // Transcript
   this.quillTranscript = new Quill(this.view.querySelector(".text"), {
     readOnly: true
   });
+
 });
 
 MapConversationAudioSourceController.defineMethod("updateView", function updateView() {
   if (!this.view) return;
 
-  var displayPlayer = function displayPlayer(display) {
+  // Audio player
+
+  let displayPlayer = function displayPlayer(display) {
     this.querySelector(".plyr").hidden = !display;
   }.bind(this.view);
 
   displayPlayer(false);
   this.source.getFileStats()
     .then(function (stats) {
-      if (this.querySelector("audio").canPlayType(stats.type) != "") {
+      if (this.querySelector("audio").canPlayType(stats.type) !== "") {
         displayPlayer(true);
       }
     }.bind(this.view));
 
-  if (this.source.fileHandler.blob) {
-    this.view.querySelector("source[data-ica-content='0']").src = this.source.fileHandler.url;
+  let url = this.source.fileHandler.url;
+  if (url) {
+    this.view.querySelector("source[data-ica-content='0']").src = url;
 
-    this.view.querySelector("a[data-ica-content='0']").href = this.source.fileHandler.url;
-    this.view.querySelector("a[data-ica-content='0']").textContent = this.source.fileHandler.url;
+    this.view.querySelector("a[data-ica-content='0']").href = url;
+    this.view.querySelector("a[data-ica-content='0']").textContent = url;
   }
 
+  // Transcript
+
   this.quillTranscript.setText(this.source.content["1"] || "");
+
+});
+
+MapConversationAudioSourceController.defineMethod("uninitView", function uninitView() {
+  if (!this.view) return;
+
+  // Transcript
+  delete this.quillTranscript;
+
 });
