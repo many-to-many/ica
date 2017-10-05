af0d94d5925a27b6b462ce8730b8b69fb1688a50
Zhuo Lu <sethlu@mintkit.net>
2017-10-05 02:52:49 -0700

    Review and annotate Notifications

--- scripts/notifications/Notifications.js
+++ scripts/notifications/Notifications.js
@@ -1,14 +1,23 @@
 
-var Notifications = JointModel.createComponent("Notifications");
+/**
+ * Notifications
+ * Concrete model for a notifications container.
+ * @constructor
+ */
+let Notifications = JointModel.createComponent("Notifications");
 
 Notifications.defineMethod("construct", function construct() {
+
   // Construct notifications
   this.notifications = [];
+
 });
 
 Notifications.defineMethod("destruct", function destruct() {
+
   // Destruct notifications
   delete this.notifications;
+
 });
 
 Notifications.prototype.addNotification = function (notification) {
@@ -17,7 +26,7 @@ Notifications.prototype.addNotification = function (notification) {
 };
 
 Notifications.prototype.removeNotification = function (notification) {
-  var index = this.notifications.indexOf(notification);
+  let index = this.notifications.indexOf(notification);
   if (index > -1) {
     this.notifications.splice(index, 1);
     this.releaseModel(notification);
