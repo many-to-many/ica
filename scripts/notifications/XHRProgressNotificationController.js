04dda866dd5c5b37994d43bf1188b2df75490a5d
Zhuo Lu <sethlu@mintkit.net>
2017-10-28 03:08:28 -0700

    Review and annotate XHRProgressNotificationController

--- scripts/notifications/XHRProgressNotificationController.js
+++ scripts/notifications/XHRProgressNotificationController.js
@@ -1,4 +1,9 @@
 
-var XHRProgressNotificationController = ProgressNotificationController.createComponent("XHRProgressNotificationController");
+/**
+ * XHRProgressNotificationController
+ * Concrete view controller to display a XHR progress notification.
+ * @constructor
+ */
+let XHRProgressNotificationController = ProgressNotificationController.createComponent("XHRProgressNotificationController");
 
 XHRProgressNotificationController.createViewFragment = ProgressNotificationController.createViewFragment;
