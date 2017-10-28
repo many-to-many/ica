c0ef33d6e133ed7eee08431be90730208fc4888c
Zhuo Lu <sethlu@mintkit.net>
2017-10-28 03:08:00 -0700

    Review and annotate ProgressNotificationController

--- scripts/notifications/ProgressNotificationController.js
+++ scripts/notifications/ProgressNotificationController.js
@@ -1,5 +1,10 @@
 
-var ProgressNotificationController = NotificationController.createComponent("ProgressNotificationController");
+/**
+ * ProgressNotificationController
+ * Concrete view controller to display a progress notification.
+ * @constructor
+ */
+let ProgressNotificationController = NotificationController.createComponent("ProgressNotificationController");
 
 ProgressNotificationController.createViewFragment = function () {
   return cloneTemplate("#template-notification-progress");
@@ -10,7 +15,7 @@ ProgressNotificationController.defineMethod("updateView", function updateView() 
 
   this.view.querySelector(".meter > .meter-bar").style.width = this.notification.progressPct * 100 + "%";
 
-  if (this.notification.progressPct == 1) {
+  if (this.notification.progressPct === 1) {
     setTimeout(function (notification) {
       notification.destroy(true, true);
     }.bind(null, this.notification), 300);
