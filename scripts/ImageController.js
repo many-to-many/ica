3143f8898213d52870f86efa10bb1af55f983e43
Zhuo Lu <sethlu@mintkit.net>
2017-11-03 19:21:38 -0700

    Review and annotate ImageController
    Resolved redundancy of nested DOM structure

--- scripts/ImageController.js
+++ scripts/ImageController.js
@@ -1,5 +1,9 @@
 
-var ImageController = SingleModelController.createComponent("ImageController");
+/**
+ * ImageController
+ * Concrete view controller to display an image model.
+ */
+let ImageController = SingleModelController.createComponent("ImageController");
 
 ImageController.createViewFragment = function () {
   return cloneTemplate("#template-image");
@@ -7,32 +11,13 @@ ImageController.createViewFragment = function () {
 
 ImageController.defineAlias("model", "fileHandler");
 
-ImageController.defineMethod("initView", function initView() {
+ImageController.defineMethod("updateView", function () {
   if (!this.view) return;
-
-});
-
-ImageController.defineMethod("updateView", function updateView() {
-  if (!this.view) return;
-
-  var parentNode = this.view.parentNode;
-  var fragment = ImageController.createViewFragment();
-  var element = fragment.querySelector(".image");
-  parentNode.replaceChild(fragment, this.view);
-  this.uninitView();
-  this._view = element;
-  this.initView(false);
 
   // Update the source of the view
-  var img = this.view.querySelector(".image-view");
-  img.src = this.fileHandler.blob
+  this.view.src = this.fileHandler.blob
     ? this.fileHandler.blob instanceof Blob
       ? this.fileHandler.url
-      : this.fileHandler.url + "?width=" + (img.offsetWidth * this.devicePixelRatio)
+      : this.fileHandler.url + "?width=" + (this.view.offsetWidth * this.devicePixelRatio)
     : "";
-});
-
-ImageController.defineMethod("uninitView", function uninitView() {
-  if (!this.view) return;
-
 });
