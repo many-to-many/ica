eaf3a6568ac94ade5e3ff1e30490a71a35cedeea
Zhuo Lu <sethlu@mintkit.net>
2017-11-03 19:14:17 -0700

    Review and annotate Controller (extension)

--- scripts/Controller.js
+++ scripts/Controller.js
@@ -1,28 +1,30 @@
 
+// Controller extension
+
 Controller.defineMethod("initView", function initView(updateView = []) {
   if (!this.view) return;
   this.view.controller = this;
 
-  var element = this.view;
+  let element = this.view;
 
   // Init textarea
   element.querySelectorAll("textarea").forEach(function (element) {
     if (element._textareaInit) return;
 
-    var style = window.getComputedStyle(element);
-    var defaultRows = element.getAttribute("rows") || 1;
+    let style = window.getComputedStyle(element);
+    let defaultRows = element.getAttribute("rows") || 1;
 
     element.rows = defaultRows;
-    var paddingTop = parseInt(style.paddingTop);
-    var paddingBottom = parseInt(style.paddingBottom);
-    var lineHeight = parseInt(style.lineHeight);
+    let paddingTop = parseInt(style.paddingTop);
+    let paddingBottom = parseInt(style.paddingBottom);
+    let lineHeight = parseInt(style.lineHeight);
 
-    var resizeTextArea = function () {
+    let resizeTextArea = function () {
       this.rows = defaultRows; // Reset to 1 for scroll height detection
-      if (this.scrollHeight == 0) return;
+      if (this.scrollHeight === 0) return;
 
-      var rows = Math.round((this.scrollHeight - paddingTop - paddingBottom) / lineHeight);
-      var maxRows = getElementProperty(this, "data-ica-max-rows");
+      let rows = Math.round((this.scrollHeight - paddingTop - paddingBottom) / lineHeight);
+      let maxRows = getElementProperty(this, "data-ica-max-rows");
       if (maxRows) this.rows = Math.min(rows, maxRows);
       else this.rows = rows;
     }.bind(element);
@@ -40,22 +42,23 @@ Controller.defineMethod("initView", function initView(updateView = []) {
   element.querySelectorAll("[data-ica-format='tokens']").forEach(function (element) {
     if (element._formatInit) return;
 
-    var tokenInputHandler = new TokenInputHandler(element);
+    let tokenInputHandler = new TokenInputHandler(element);
 
-    var tokenInputFragment = TokenInputController.createViewFragment();
-    var tokenInputElement = tokenInputFragment.querySelector(".tokens");
+    let tokenInputFragment = TokenInputController.createViewFragment();
+    let tokenInputElement = tokenInputFragment.querySelector(".tokens");
     element.parentNode.insertBefore(tokenInputFragment, element);
-    var tokenInputController = new TokenInputController(tokenInputHandler, tokenInputElement);
+    let tokenInputController = new TokenInputController(tokenInputHandler, tokenInputElement);
     if (element.controller) tokenInputController.componentOf = element.controller;
 
     switch (getElementProperty(element, "suggestions")) {
-      case "themes":
-        var tokenSuggestionsFragment = TokenInputSuggestionsController.createViewFragment();
-        var tokenSuggestionsElement = tokenSuggestionsFragment.querySelector(".tokens");
+      case "themes": {
+        let tokenSuggestionsFragment = TokenInputSuggestionsController.createViewFragment();
+        let tokenSuggestionsElement = tokenSuggestionsFragment.querySelector(".tokens");
         element.parentNode.insertBefore(tokenSuggestionsFragment, element.nextSibling);
-        var tokenSuggestionsController = new TokenInputThemeSuggestionsController(tokenInputHandler, tokenSuggestionsElement);
+        let tokenSuggestionsController = new TokenInputThemeSuggestionsController(tokenInputHandler, tokenSuggestionsElement);
         if (element.controller) tokenSuggestionsController.componentOf = element.controller;
         break;
+      }
     }
 
     element._formatInit = true;
@@ -108,6 +111,8 @@ Controller.defineMethod("uninitView", function () {
   });
 
 }(Controller));
+
+
 
 function resize(container = document.body) {
   container.querySelectorAll("[data-ica-width-multiple]")
