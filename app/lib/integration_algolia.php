baba6d4b7b756d68fd8ec9f1258412e5132178b5
Zhuo Lu <sethlu@mintkit.net>
2018-06-13 23:40:17 -0700

    Only init Algolia when configured

--- app/lib/integration_algolia.php
+++ app/lib/integration_algolia.php
@@ -8,15 +8,20 @@ namespace {
 
 namespace ICA\Algolia {
 
-  /**
-   * @throws \Exception
-   */
   function init() {
 
-    $client = new \AlgoliaSearch\Client(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
+    if (!defined("ALGOLIA_APP_ID") ||
+        !defined("ALGOLIA_API_KEY") ||
+        !defined("ALGOLIA_INDEX")) return;
 
     global $ALGOLIA_INDEX;
-    $ALGOLIA_INDEX = $client->initIndex(ALGOLIA_INDEX);
+
+    try {
+      $client = new \AlgoliaSearch\Client(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
+      $ALGOLIA_INDEX = $client->initIndex(ALGOLIA_INDEX);
+    } catch (\Exception $e) {
+      die("Failed to initialize Algolia integration");
+    }
 
   }
 
